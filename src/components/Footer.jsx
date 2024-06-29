6f3c1999fca77b11da63c91c3743b22882c42ff8
shiva1240 <ss8721240@gmail.com>
2024-06-29 11:35:30 +0530

    change in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -20,9 +20,8 @@ function Footer() {
             </div>
             <div className="mt-8 border-t border-gray-700 pt-8 flex flex-col items-center">
               <p className="text-sm">
-                &copy; 2024 Your Company. All rights reserved.
+                 All rights reserved.
               </p>
-              <p className="text-sm">Supportive Partner ❤️ Ankush</p>
             </div>
           </div>
         </div>
